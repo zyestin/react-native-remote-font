65ccdfacbccbf8d0ff4f44006017e425dc3d0168
zhaoyang <zhaoyang@ihuaben.com>
2023-12-15 12:04:44 +0800

    feat: add Android code. test ok on iPhone XR.

--- android/src/main/java/com/yestin/font/RemoteFontLoaderModule.java
+++ android/src/main/java/com/yestin/font/RemoteFontLoaderModule.java
@@ -6,6 +6,15 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+
+import android.graphics.Typeface;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+
+import java.io.File;
 
 public class RemoteFontLoaderModule extends ReactContextBaseJavaModule {
 
@@ -22,8 +31,37 @@ public class RemoteFontLoaderModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
-    }
+    public void loadFontFile(String pFontPath, int viewTargetId,  Promise pPromise) {
+      if (pFontPath != null) {
+          final View lView = this.getReactApplicationContext().getCurrentActivity().findViewById(viewTargetId);
+          if (lView != null) {
+              try {
+                  File fontFile = new File(pFontPath);
+                  if (fontFile.exists()) {
+                      Typeface typeface = Typeface.createFromFile(fontFile);
+                      // Attempt to coerce the TextView.
+                      TextView pTextView = (TextView) (lView);
+                      // Assign the Typeface.
+                      pTextView.setTypeface(typeface);
+                      // Ensure we update the View layout.
+                      pTextView.measure(View.MeasureSpec.makeMeasureSpec(pTextView.getMeasuredWidth()-100, View.MeasureSpec.EXACTLY),
+                              View.MeasureSpec.makeMeasureSpec(pTextView.getMeasuredHeight(), View.MeasureSpec.EXACTLY));
+                      pTextView.layout(0, 0, pTextView.getMeasuredWidth(), pTextView.getMeasuredHeight());
+                      pPromise.resolve(Arguments.createMap());
+                  } else {
+                      pPromise.reject("Error", "File not exists");
+                  }
+
+              } catch (final Exception pException) {
+                  // Delegate the Exception to the caller.
+                  pPromise.reject(pException);
+              }
+          }else{
+              pPromise.reject("Error", viewTargetId + " view is not found");
+          }
+
+      }else{
+          pPromise.reject("Error", "font filepath is null");
+      }
+  }
 }
--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+// react-native-fs
+import com.rnfs.RNFSPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+            packages.add(new RNFSPackage());
           return packages;
         }
 
